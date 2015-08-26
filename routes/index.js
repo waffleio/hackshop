6133983e679a5f44baf23979a0f9e8d5439f9fec
Andrew Homeyer <homeyer@gmail.com>
2015-08-25 23:55:10 -0600

    fix title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'NC Open Data Hackshop | Waffle.io' });
 });
 
 module.exports = router;
