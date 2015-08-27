ea6d1b9225c6969de125804e9d8c6947090efcf7
Andrew Homeyer <homeyer@gmail.com>
2015-08-27 16:57:15 -0600

    using bower for client side dependencies

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(require('cookie-session')({ secret: process.env.HACKSHOP_SESSION_SECRET})); // openssl rand -base64 32
 
 passport = require('./passport').init(app);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NC Open Data Hackshop | Waffle.io' });
+  res.render('index', {
+  	title: 'NC Open Data Hackshop | Waffle.io',
+  	session: _.pick(req.user, ['accessToken', 'avatar', 'username'])
+  });
 });
 
 module.exports = router;
