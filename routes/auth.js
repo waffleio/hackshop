2e87f82414f9f60464109b1d7ba6942722597008
Mary Davis <mdavis@mdavis-macbook.f4tech.com>
2015-09-10 19:58:42 -0400

    fixed redirect

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 router.get('/waffle', passport.authenticate('waffle', {scope: '*'}));
 router.get('/waffle/callback',
 passport.authenticate('waffle', { failureRedirect: '/' }), function(req, res) {
-  res.redirect('/#steps');
+  res.redirect('/');
 });
 
 module.exports = router;
